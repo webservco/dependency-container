b079ac0d358f3427a454e78d4c59618c905e36d9
Radu Maier <ariademur@webserv.co>
2023-07-22 17:45:43 +0200

    AppDepCon notes

--- src/WebServCo/DependencyContainer/Contract/ApplicationDependencyContainerInterface.php
+++ src/WebServCo/DependencyContainer/Contract/ApplicationDependencyContainerInterface.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace WebServCo\DependencyContainer\Contract;
 
+/**
+ * General application level dependency container.
+ *
+ * Common for all projects using this component.
+ * Implementation also provided.
+ */
 interface ApplicationDependencyContainerInterface extends DependencyContainerInterface
 {
     public function getFactoryContainer(): FactoryContainerInterface;
