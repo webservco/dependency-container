859a85ee9f79cefac5ada0189c4fe805aa6e5cfa
Radu Maier <ariademur@webserv.co>
2023-07-23 20:13:25 +0200

    LocalDependencyContainerFactoryInterface

---
+++ src/WebServCo/DependencyContainer/Contract/LocalDependencyContainerFactoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebServCo\DependencyContainer\Contract;
+
+interface LocalDependencyContainerFactoryInterface
+{
+    public function createLocalDependencyContainer(
+        ApplicationDependencyContainerInterface $applicationDependencyContainer,
+    ): LocalDependencyContainerInterface;
+}
